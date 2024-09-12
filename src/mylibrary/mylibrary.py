68185525b5aae1ecb1c715aeff0e10972ff0a1b2
EnzoVCode <enzo.volpato09@gmail.com>
2024-09-12 12:28:44 +0200

    feat(mylibrary): add hello_world3 function

---
+++ src/mylibrary/mylibrary.py
@@ -0,0 +1,11 @@
+# src/mylibrary/mylibrary.py
+def hello_world():
+    return "Hello World"
+
+def hello_world2():
+    """Returns the string 'Hello World 2'."""
+    return "Hello World 2"
+
+def hello_world3():
+        """Returns the string 'Hello World 3'."""
+    return "Hello World 3"
